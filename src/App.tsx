16ca151f4fc40e47c259944280d890747d213879
Dima <d@no.de>
2021-11-03 11:40:50 +0100

    Texte geändert auf Startseite.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          In der App.tsx könnt ihr jetzt mit eurer Anwendung anfangen!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lernt React mit mir!
         </a>
       </header>
     </div>
